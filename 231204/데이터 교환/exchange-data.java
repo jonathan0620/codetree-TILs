d67df0c5bc7d346a29a2c652c149f94fb238bc86
jonathan <145638699+jonathan0620@users.noreply.github.com>
2023-12-04 14:24:07 +0900

    Title: 데이터 교환, Time: 89ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 231204/데이터 교환/exchange-data.java
+++ 231204/데이터 교환/exchange-data.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         int a = 5; int b = 6; int c = 7;
-        c = b; a = c;
+        b=c; c=a;
         System.out.println(a);
         System.out.println(b);
         System.out.print(c);
