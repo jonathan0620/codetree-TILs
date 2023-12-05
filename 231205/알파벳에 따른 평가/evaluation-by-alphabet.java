f4f4466a9111d126ba904017b5763faa6574658e
jonathan <145638699+jonathan0620@users.noreply.github.com>
2023-12-05 22:47:24 +0900

    Title: 알파벳에 따른 평가, Time: 102ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 231205/알파벳에 따른 평가/evaluation-by-alphabet.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        char c = s.charAt(0);
+
+        if (c == 'S') {
+            System.out.println("Superior");
+        }
+        else if (c == 'A') {
+            System.out.println("Excellent");
+        }
+        else if (c == 'B') {
+            System.out.println("Good");
+        }
+        else if (c == 'C') {
+            System.out.println("Usually");
+        }
+        else if (c == 'E') {
+            System.out.println("Effort"); 
+        }else{
+            System.out.println("Failure");
+        }
+
+    }
+}
