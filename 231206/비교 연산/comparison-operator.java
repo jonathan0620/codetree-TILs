82938f648638addc9613f997410d0c3d10bb11b0
jonathan <145638699+jonathan0620@users.noreply.github.com>
2023-12-06 13:09:07 +0900

    Title: 비교 연산, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 231206/비교 연산/comparison-operator.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        if(a>=b){
+            System.out.println(1);
+        }else{
+            System.out.println(0);
+        }
+        if(a>b){
+            System.out.println(1);
+        }else{
+            System.out.println(0);
+        }
+        if(b>=a){
+            System.out.println(1);
+        }else{
+            System.out.println(0);
+        }
+        if(b>a){
+            System.out.println(1);
+        }else{
+            System.out.println(0);
+        }
+        if(a==b){
+            System.out.println(1);
+        }else{
+            System.out.println(0);
+        }
+        if(a!=b){
+            System.out.println(1);
+        }else{
+            System.out.println(0);
+        }
+    }
+}
