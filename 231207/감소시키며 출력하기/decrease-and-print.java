281b8943568c6641f272fac83b3a85ba49f63bc1
jonathan <145638699+jonathan0620@users.noreply.github.com>
2023-12-07 09:17:17 +0900

    Title: 감소시키며 출력하기, Time: 79ms, Memory: 8MB, Status: Passed - CodeTree

--- 231207/감소시키며 출력하기/decrease-and-print.java
+++ 231207/감소시키며 출력하기/decrease-and-print.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        for (int i = 17; i > 5; i--) {
+        for (int i = 17; i > 4; i--) {
             System.out.print(i+" ");
         }
 
