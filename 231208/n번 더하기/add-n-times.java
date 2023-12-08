ef75e2660cfc753ef5140eb7910e9441a3050e54
jonathan <145638699+jonathan0620@users.noreply.github.com>
2023-12-08 09:20:10 +0900

    Title: n번 더하기, Time: 124ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 231208/n번 더하기/add-n-times.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        // 변수 선언
+        int a, b, c;
+
+        // 입력
+        a = sc.nextInt();
+        b = sc.nextInt();
+        c = a+b;
+        for(int i = 0; i < b; i++){
+            System.out.println(c);
+            c = c+b;
+        }
+    }
+}
