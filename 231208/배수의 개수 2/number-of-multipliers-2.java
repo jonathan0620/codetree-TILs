769e90ed997a91675ae9bc21e4ed316a11f94d08
jonathan <145638699+jonathan0620@users.noreply.github.com>
2023-12-08 15:10:20 +0900

    Title: 배수의 개수 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 231208/배수의 개수 2/number-of-multipliers-2.java
+++ 231208/배수의 개수 2/number-of-multipliers-2.java
@@ -5,7 +5,7 @@ public class Main {
         int a;
         int cnt = 0;
 
-        for (int i = 1; i <= 10; i++) {
+        for (int i = 0; i <= 10; i++) {
             a = sc.nextInt();
             if (i % 2!= 0) {
                 cnt++;
