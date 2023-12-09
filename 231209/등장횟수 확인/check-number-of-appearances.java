85da71dbac491439e130fa121bb1379845e0f0d6
jonathan <145638699+jonathan0620@users.noreply.github.com>
2023-12-09 13:13:47 +0900

    Title: 등장횟수 확인, Time: 154ms, Memory: 10MB, Status: Passed - CodeTree

--- 231209/등장횟수 확인/check-number-of-appearances.java
+++ 231209/등장횟수 확인/check-number-of-appearances.java
@@ -5,11 +5,8 @@ public class Main {
         int cnt = 0; 
         for (int i = 1; i <= 5; i++) {
             int a = sc.nextInt();
-            if (i % 2 == 0) {
+            if (a % 2 == 0) {
                 cnt++;
-                if(a == 1){
-                    cnt =0;
-                }
             }
         }
         System.out.print(cnt);
