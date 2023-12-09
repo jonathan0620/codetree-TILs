0fa3c1b453e1629abd22b78d117f40558617f9b8
jonathan <145638699+jonathan0620@users.noreply.github.com>
2023-12-09 11:46:34 +0900

    Title: 등장횟수 확인, Time: 111ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 231209/등장횟수 확인/check-number-of-appearances.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a;
+        int cnt = 0; 
+        for (int i = 1; i <= 5; i++) {
+            a = sc.nextInt();
+            if (i % 2 == 0) {
+                cnt++;
+            }
+        }
+        System.out.print(cnt);
+    }
+}
