69972f669c84c767c9b6063a1a9edf852aea7f1f
jonathan <145638699+jonathan0620@users.noreply.github.com>
2023-12-12 09:50:56 +0900

    Title: 합성수 판별기, Time: 146ms, Memory: 10MB, Status: Passed - CodeTree

--- 231212/합성수 판별기/factorization-discriminator.java
+++ 231212/합성수 판별기/factorization-discriminator.java
@@ -7,7 +7,7 @@ public class Main {
         
         boolean satisfied = false;
 
-        for (int i = 2; i <= a; i++) {
+        for (int i = 2; i <=Math.sqrt(a); i++) {
             if (a % i == 0) {
                 satisfied = true;
                 break;
