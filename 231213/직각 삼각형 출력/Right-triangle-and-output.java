07a8228758212c80a11b5a95c65911531e6965a3
jonathan <145638699+jonathan0620@users.noreply.github.com>
2023-12-13 13:40:14 +0900

    Title: 직각 삼각형 출력, Time: 130ms, Memory: 10MB, Status: Passed - CodeTree

--- 231213/직각 삼각형 출력/Right-triangle-and-output.java
+++ 231213/직각 삼각형 출력/Right-triangle-and-output.java
@@ -3,13 +3,11 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // 사용자로부터 n을 입력 받습니다.
         int n = scanner.nextInt();
 
-        // 1부터 n까지 반복하면서 각 줄에 해당하는 별을 출력합니다.
-        for (int i = 1; i <= n; i++) {
-            // 각 줄에는 (2 * i - 1)개의 별이 출력됩니다.
-            for (int j = 1; j <= 2 * i - 1; j++) {
+        for (int i = 0; i < n; i++) {
+         
+            for (int j = 0; j < 2 * i + 1; j++) {
                 System.out.print("*");
             }
             // 줄 바꿈을 추가하여 다음 줄로 이동합니다.
