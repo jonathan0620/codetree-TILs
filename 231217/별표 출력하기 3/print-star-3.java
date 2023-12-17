7272ca622acc91716d23a990ef1ec8fccd9f1201
jonathan <145638699+jonathan0620@users.noreply.github.com>
2023-12-17 20:35:12 +0900

    Title: 별표 출력하기 3, Time: 161ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 231217/별표 출력하기 3/print-star-3.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        
+        // 별표 출력
+        printStars(n);
+    }
+
+    // 별표 출력 함수
+    static void printStars(int n) {
+        for (int i = 0; i < n; i++) {
+            // 공백 출력
+            for (int j = 0; j < i; j++) {
+                System.out.print("  ");
+            }
+
+            // 별표 출력
+            for (int j = 0; j < 2 * (n - i) - 1; j++) {
+                System.out.print("* ");
+            }
+
+            // 줄 바꿈
+            System.out.println();
+        }
+    }
+}
