0cfa2989136938c8c82e703ca9838dfc692d803f
jonathan <145638699+jonathan0620@users.noreply.github.com>
2023-12-18 11:07:00 +0900

    Title: 별표 출력하기 6, Time: 137ms, Memory: 10MB, Status: Passed - CodeTree

--- 231218/별표 출력하기 6/print-star-6.java
+++ 231218/별표 출력하기 6/print-star-6.java
@@ -7,41 +7,41 @@ public class Main {
         // 정수 n 입력 받기
         int n = scanner.nextInt();
 
-        // n에 따른 별표 출력
+        // 별표 출력
         printStarPattern(n);
-
-        scanner.close();
     }
 
     // 별표 출력 메서드
     private static void printStarPattern(int n) {
+        // 상단 부분 출력
         for (int i = 0; i < n; i++) {
-            // 왼쪽 공백 출력
+            // 공백 출력
             for (int j = 0; j < i; j++) {
                 System.out.print("  ");
             }
 
-            // 왼쪽 별표 출력
-            for (int j = 0; j < n - i; j++) {
+            // 별표 출력
+            for (int j = 0; j < 2 * (n - i) - 1; j++) {
                 System.out.print("* ");
             }
 
-            // 다음 줄로 이동
+            // 줄 바꿈
             System.out.println();
         }
 
+        // 하단 부분 출력
         for (int i = 1; i < n; i++) {
-            // 왼쪽 공백 출력
-            for (int j = 1; j < n - i; j++) {
+            // 공백 출력
+            for (int j = 0; j < n - i - 1; j++) {
                 System.out.print("  ");
             }
 
-            // 왼쪽 별표 출력
-            for (int j = 0; j <= i; j++) {
+            // 별표 출력
+            for (int j = 0; j < 2 * i + 1; j++) {
                 System.out.print("* ");
             }
 
-            // 다음 줄로 이동
+            // 줄 바꿈
             System.out.println();
         }
     }
